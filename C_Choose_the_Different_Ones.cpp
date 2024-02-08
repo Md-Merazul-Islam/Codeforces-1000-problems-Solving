b077c14e39a164a0b189f5a114e76a9bad9b0f31
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-02-08 15:59:17 +0600

    cf -1000+

--- C_Choose_the_Different_Ones.cpp
+++ C_Choose_the_Different_Ones.cpp
@@ -4,8 +4,8 @@
 **             https://github.com/Md-Merazul-Islam           **
 **************************************************************/
 #include <bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 using namespace std;
 #define PI 3.14159265359
@@ -17,7 +17,7 @@ using namespace std;
 #define pb push_back
 #define vi vector<ll>
 #define vpi vector<pair<ll, ll>>
-#define pii pair<ll,ll> 
+#define pii pair<ll, ll>
 #define srt(a) sort(a.begin(), a.end())
 #define all(a) a.begin(), a.end()
 #define rsrt(a) sort(a.rbegin(), a.rend())
@@ -26,40 +26,43 @@ using namespace std;
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
 #define loop(i, a, b) for (int i = (a); i < (b); ++i)
 #define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
-#define scan(a) loop(i,0, a.size()) cin >> a[i]
-void print (vector<ll>&a) {loop(i,0,a.size())cout<<a[i]<<' ';}
-template <typename T> using my_ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-const int N = 2e5 +5;
+#define scan(a) loop(i, 0, a.size()) cin >> a[i]
+void print(vector<ll> &a) { loop(i, 0, a.size()) cout << a[i] << ' '; }
+template <typename T>
+using my_ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+const int N = 2e5 + 5;
 void hello_world_solve_here()
 {
-    
-    ll n;cin>>n;
-    string a;cin>>a;
-    ll aa,b;
-    if(n==1){
-        cout<<1<<endl;return;
-    }
-    for (int i=0; i<n; i++)
-    {
-        if(a[i]=='B'){
-            aa=i;break;
-        }
-    }
-    for (int i=n-1; i>=0; i--)
-    {
-        if(a[i]=='B'){
-            b= i;break;
-        }
-    }
-    cout<<abs(aa-b)+1<<endl;
-}
 
+    int n, m, k;
+    cin >> n >> m >> k;
+    vi a(n), b(m);
+    scan(a);
+    scan(b);
+   vector<int> cnt(k + 1, 0);
+    for (int e : a) {
+        if (e <= k) cnt[e] |= 1;
+    }
+    for (int e : b) {
+        if (e <= k) cnt[e] |= 2;
+    }
+    vector<int> c(4, 0);
+    for (int e : cnt) {
+        c[e]++;
+    }
+    if (c[1] > k / 2 || c[2] > k / 2 || c[1] + c[2] + c[3] != k) {
+       no;
+    } else {
+        yes;
+    }
+}
 
 signed main()
 {
     fast;
-    int t=1;
-        cin>>t;
-    while(t--)hello_world_solve_here();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        hello_world_solve_here();
     return 0;
 }
