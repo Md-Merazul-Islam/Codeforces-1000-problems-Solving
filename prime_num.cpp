4f9d6ad45e47eec5978f129005e3543b5f76b6c3
Md Merazul Islam <129538030+Md-Merazul-Islam@users.noreply.github.com>
2024-11-23 10:01:36 +0600

    git pu

---
+++ prime_num.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int lower, upper;
+    cout << "Enter the lower limit: ";
+    cin >> lower;
+    cout << "Enter the upper limit: ";
+    cin >> upper;
+
+    for (int i = lower; i < upper; i++)
+    {
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                break;
+            }
+            else
+            {
+                cout << i << endl;
+            }
+        }
+    }
+
+    return 0;
+}
