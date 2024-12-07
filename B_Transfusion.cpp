ce049d5e1b57a7a2d24a2fbee9db6e55209e811d
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-12-07 22:49:34 +0600

    solved b

--- B_Transfusion.cpp
+++ B_Transfusion.cpp
@@ -1,60 +1,64 @@
-#include <iostream>
-#include <vector>
+
+#include <bits/stdc++.h>
 using namespace std;
+#define PI 3.14159265359
+#define dmin 1e-9
+#define dd double
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define pb push_back
+#define vi vector<ll>
+#define vpi vector<pair<ll, ll>>
+#define pii pair<ll,ll> 
+#define srt(a) sort(a.begin(), a.end())
+#define all(a) a.begin(), a.end()
+#define rsrt(a) sort(a.rbegin(), a.rend())
+#define line "\n"
+#define nl cout << "\n"
+#define out(x) cout << x << "\n";
+#define loop(i, a, b) for (int i = (a); i < (b); ++i)
+#define rloop(i, a, b) for (int i = (a); i <= (b); ++i)
+#define scan(a) loop(i,0, a.size()) cin >> a[i]
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+const int N = 2e5 +5;
 
-int main()
+void hello_world_solve_here()
 {
-    int t; // Number of test cases
-    cin >> t;
-
-    while (t--)
-    {
-        int n; // Length of the array
-        cin >> n;
-
-        vector<int> a(n);
-        long long sum = 0; // To store the sum of elements
-
-        // Input the array and calculate the sum
-        for (int i = 0; i < n; ++i)
-        {
-            cin >> a[i];
-            sum += a[i];
-        }
-
-        // Check if the sum is divisible by n
-        if (sum % n != 0)
-        {
-            cout << "NO" << endl;
-        }
-        else
-        {
-            // Check if redistribution can work
-            bool possible = true;
-            long long target = sum / n;
-            long long balance = 0;
-
-            // Traverse through the array to check if we can balance the elements
-            for (int i = 0; i < n; ++i)
-            {
-                balance += a[i] - target;
-                if (balance < 0)
-                {
-                    possible = false;
-                    break;
-                }
-            }
-
-            if (possible)
-            {
-                cout << "YES" << endl;
-            }
-            else
-            {
-                cout << "NO" << endl;
-            }
-        }
+    
+    ll n;cin>>n;
+    vi a(n);
+    ll sum =0;
+    for (ll i = 0; i < n; ++i){
+        cin>>a[i];
+        sum += a[i];
+    }
+    if(sum%n!=0){
+        no;return;
     }
 
+    ll target = sum /n;
+    ll evenSum =0, oddSum=0;
+    ll even=0, odd=0;
+    for (ll i=0; i<n; i+=2){
+        evenSum+=a[i];
+        even++;
+    }
+    for (ll i=1; i<n; i+=2){
+        oddSum+=a[i];
+        odd++;
+    }
+
+    if(evenSum/even!=target || oddSum/odd!=target)no;
+    else yes;
+}
+
+
+signed main()
+{
+    fast;
+    int t=1;
+        cin>>t;
+    while(t--)hello_world_solve_here();
     return 0;
 }
