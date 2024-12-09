d2c861a2783c7b854f50dbb75acfb3367dde7272
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-12-09 15:44:58 +0600

    codeforces

--- C_Uninteresting_Number.cpp
+++ C_Uninteresting_Number.cpp
@@ -6,7 +6,22 @@ using namespace std;
 
 bool isDivisiable(string &s)
 {
-    
+    ll sum = 0, a_2 = 0, b_2 = 0;
+    for (auto c : s)
+    {
+        a_2 += (c == '2');
+        b_2 += (c == '3');
+        sum += (c - '0');
+    }
+    for (ll i = 0; i <= min(a_2, (long long)9); i++)
+    {
+        for (ll j = 0; j <= min(b_2, (long long)9); j++)
+        {
+            if ((sum + i * 2 + j * 6) % 9 == 0)
+                return true;
+        }
+    }
+    return false;
 }
 
 int main()
@@ -17,6 +32,10 @@ int main()
     {
         string s;
         cin >> s;
+        if (isDivisiable(s))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
 
     return 0;
